4180c2082fe8c656a869a2282837f14d66150382
dcaresg <diegopatricioc@gmail.com>
2026-03-14 17:15:03 -0300

    feat: add API Gateway endpoint

--- cdk/lambda/lib/lambda-stack.ts
+++ cdk/lambda/lib/lambda-stack.ts
@@ -1,15 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import * as path from 'path';
 import { Construct } from 'constructs';
-
-
 
 export class LambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // Tabla DynamoDB
     const tabla = new dynamodb.Table(this, 'MiTabla', {
       tableName: 'mi-tabla',
       partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
@@ -17,6 +17,7 @@ export class LambdaStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     });
 
+    // Lambda
     const holaFn = new lambda.Function(this, 'HolaMundoFunction', {
       runtime: lambda.Runtime.NODEJS_20_X,
       handler: 'handler.handler',
@@ -28,7 +29,23 @@ export class LambdaStack extends cdk.Stack {
       },
     });
 
+    // Permisos
     tabla.grantReadData(holaFn);
+
+    // API Gateway
+    const api = new apigateway.RestApi(this, 'HolaMundoApi', {
+      restApiName: 'hola-tu-api',
+      description: 'API para Hola Mundo',
+    });
+
+    const holaResource = api.root.addResource('hola');
+    holaResource.addMethod('GET', new apigateway.LambdaIntegration(holaFn));
+
+    // Outputs
+    new cdk.CfnOutput(this, 'ApiUrl', {
+      value: `${api.url}hola`,
+      description: 'URL del endpoint',
+    });
 
     new cdk.CfnOutput(this, 'LambdaArn', {
       value: holaFn.functionArn,
